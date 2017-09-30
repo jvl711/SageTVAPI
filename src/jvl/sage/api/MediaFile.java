0c8896c6c48ec539ef5ce14d80e3803b3fa353db
jvl711 <jvl711@970a8ef4-2458-a846-aee5-a606d86be16f>
2017-09-30 12:58:08 +0000

    Updated commercial marker to be passed the right times
    git-svn-id: https://svn.joshandmonique.com/svn/SageTV/SageAPI@125 970a8ef4-2458-a846-aee5-a606d86be16f

--- src/jvl/sage/api/MediaFile.java
+++ src/jvl/sage/api/MediaFile.java
@@ -244,7 +244,7 @@ public class MediaFile extends SageObject
                     long startTime = (long)(Double.parseDouble(cuttimes[0]) * 1000);
                     long endTime = (long)(Double.parseDouble(cuttimes[1]) * 1000);
                    
-                    Marker marker = new Marker(startTime, endTime, segments[i].GetStartTime(), segments[i].GetStartTime(), segments[i].GetEndTime());
+                    Marker marker = new Marker(startTime, endTime, segments[i].GetStartTime(), this.GetFileStartTime(), this.GetFileEndTime());
                     temp.add(marker);
                 }                
             }
