9a375b8711d8920cdf4c2272935e0122cc9f4884
jvl711 <jvl711@970a8ef4-2458-a846-aee5-a606d86be16f>
2018-05-05 13:15:36 +0000

    Added first implementation of UITrhead.
    git-svn-id: https://svn.joshandmonique.com/svn/SageTV/SageAPI@443 970a8ef4-2458-a846-aee5-a606d86be16f

--- src/jvl/sage/UIThread.java
+++ src/jvl/sage/UIThread.java
@@ -3,17 +3,19 @@ package jvl.sage;
 
 import jvl.sage.api.Widget;
 
-
 public class UIThread extends Thread
 {
     private Widget execute;
     public Object executeResult;
     private Widget beforeExecute;
     private Widget afterExecute;
-    
-    public UIThread()
+    private final long timer;
+    private boolean running;
+
+    public UIThread(Widget executeWidget, long timer)
     {
-        
+        this.execute = executeWidget;
+        this.timer = timer;
     }
     
     public void SetOnExecuteWidget(Widget widget)
@@ -38,29 +40,59 @@ public class UIThread extends Thread
     
     public void StartThread()
     {
-        
+        this.running = true;
+        this.start();
     }
     
     public void StopThread()
     {
-        
+        this.running = false;
     }
     
     @Override
-    public void run() 
+    public void run()
     {
+
+        while(running)
+        {
+            try
+            {
+                if(this.beforeExecute != null)
+                {
+                    this.beforeExecute.ExecuteWidgetChain();
+                }
+
+            }
+            catch(Exception ex) { }
+
+            try
+            {
+                this.executeResult = this.execute.ExecuteWidgetChain();
+            }
+            catch(Exception ex)
+            {
+                /*
+                What do we do...  Do we exit thread?  Do we allow a certain number
+                of errors?
+                */
+            }
+
+            try
+            {
+                if(this.afterExecute != null)
+                {
+                    this.afterExecute.ExecuteWidgetChain();
+                }
+
+            }
+            catch(Exception ex) { }
+        }
         
         try
         {
-            this.executeResult = this.execute.ExecuteWidgetChain();
+            Thread.sleep(timer);
         }
-        catch(Exception ex)
-        {
-            /*
-            What do we do...  Do we exit thread?  Do we allow a certain number
-            of errors?
-            */
-        }
+        catch(Exception ex) { }
         
     }
 }
